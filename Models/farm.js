2b5753b1d167d0e1d3502b47d54361b2a6e63a0e
Joe Scott <jbs198585@gmail.com>
2022-05-27 06:46:29 -0400

    One to many

---
+++ Models/farm.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/relationshipDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  season: {
+    type: String,
+    enum: ['Spring', 'Summer', 'Fall', 'Winter']
+  }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+Product.insertMany([
+  {name: 'Goddess Melon', price: 4.99, season: 'Summer'},
+  {name: 'Sugar Baby Watermelon', price: 4.99, season: 'Summer'},
+  {name: 'Asparagus', price: 3.99, season: 'Spring'},
+])
